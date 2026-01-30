b0ffff60770e66a06bdf09e16a7695180904d4ec
Chris Wahlen <chris@chriswahlen.com>
2026-01-30 13:06:38 -0800

    deleted nonexistent references

--- profit/sources/__init__.py
+++ profit/sources/__init__.py
@@ -4,12 +4,6 @@ Fetcher abstractions and provider-facing types.
 
 from .base_fetcher import BaseFetcher
 from .batch_fetcher import BatchFetcher
-from .commodities.base import (
-    CommoditiesDailyFetcher,
-    CommodityDailyPrice,
-    CommodityDailyRequest,
-)
-from .commodities.goldapi import GoldApiCommoditiesFetcher
 from .types import Fingerprintable
 
 __all__ = ["BaseFetcher", "BatchFetcher", "Fingerprintable"]
