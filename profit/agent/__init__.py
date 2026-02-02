99e905609cd34c8882a0cf73a76e3d3457cfb8be
Chris Wahlen <chris@chriswahlen.com>
2026-02-02 12:32:28 -0800

    delete the #aislop so we can start over

--- profit/agent/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Lightweight agent scaffolding for routing questions to local data sources
-and composing responses via an LLM client.
-"""
-
-from .types import Question, RetrievalPlan, RetrievedData, Answer  # noqa: F401
-from .router import Router  # noqa: F401
-from .llm import ChatLLM, StubLLM  # noqa: F401
-from .planner import interpret_planner_output, PlannerDecision, SourceRequest  # noqa: F401
