bab1658899e08800cde65798f3b3dae0d9617a6c
Chris Wahlen <chris@chriswahlen.com>
2026-02-09 23:59:30 -0800

    continue starting over

--- scripts/select_unnamed_instruments.sql
+++
@@ -1,9 +0,0 @@
--- Select instruments that aren't associated with an entity
-SELECT ipm.provider,
-       ipm.provider_code AS symbol,
-       ipm.instrument_id
-FROM instrument_provider_map AS ipm
-LEFT JOIN instrument_entity AS ie
-  ON ie.instrument_id = ipm.instrument_id
-WHERE ie.instrument_id IS NULL
-ORDER BY ipm.provider, ipm.provider_code;
